f6a00098c1f9e5e403a450cafa4f3ff2089ebcc9
avillaq <villafuertequispealex@gmail.com>
2025-05-15 18:26:40 -0500

    Se añade chadcn al frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-
-  return <>
-    <div className='text-3xl font-bold underline'>
-      Hola
+  return (
+    <div >
+      <Button>Click me</Button>
     </div>
-  </>
+  )
 }
 
 export default App

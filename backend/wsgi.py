08cef559b88a8bb685337987b32f3b59b12ee04e
avillaq <villafuertequispealex@gmail.com>
2025-05-19 23:30:54 -0500

    nuevo requirements

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,15 +1,11 @@
 from app import create_app
 from flask import redirect
-from backend.app.core.extensions import db
 
 app = create_app()
 
 @app.route("/")
 def index_api():
     return redirect("/api")
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)

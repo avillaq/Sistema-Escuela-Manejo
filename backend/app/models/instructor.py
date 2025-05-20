2dfd634bb0fc2067a129fca9dbb3408363cb0e98
avillaq <villafuertequispealex@gmail.com>
2025-05-20 10:38:11 -0500

    Creacion los modelos administrador e instructor

---
+++ backend/app/models/instructor.py
@@ -0,0 +1,13 @@
+from app.extensions import db
+
+class Instructor(db.Model):
+    __tablename__ = "instructores"
+
+    id = db.Column(db.Integer, primary_key=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey("usuarios.id"), unique=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellidos = db.Column(db.String(100), nullable=False)
+    dni = db.Column(db.String(20), unique=True, nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
+
+    usuario = db.relationship("Usuario", backref="instructor", uselist=False)

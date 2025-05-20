4b0d933e58994fb47a8bdac2c749d07b5564e85a
avillaq <villafuertequispealex@gmail.com>
2025-05-20 01:05:26 -0500

    registro de algunas blueprints

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from .extensions import db, migrate, limiter, cors, guard, blacklist
-from app.models import Usuario
+from .models.usuario import Usuario
+from .routes.alumnos import alumnos_bp
+from .routes.auth import auth_bp
 
 def create_app():
     app = Flask(__name__)
@@ -13,7 +15,7 @@ def create_app():
     guard.init_app(app, Usuario,is_blacklisted=blacklist.is_blacklisted)
 
     # Registrar los blueprints
-    from app.api import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix="/api")
+    app.register_blueprint(auth_bp, url_prefix="/api/auth")
+    app.register_blueprint(alumnos_bp, url_prefix="/api/alumnos")
 
     return app

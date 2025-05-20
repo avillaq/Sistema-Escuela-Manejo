f4e630f30185d7360e91b5f40afa6a6951d062cf
avillaq <villafuertequispealex@gmail.com>
2025-05-20 01:04:29 -0500

    Configuracion del proyecto

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
   SECRET_KEY = os.getenv("SECRET_KEY")
-  SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_LOCAL')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
-  JWT_ACCESS_LIFESPAN = {"days": 2} # We don't use refresh tokens. So, we set the lifespan of the access token to 2 days.
-  JWT_REFRESH_LIFESPAN = {"days": 30}
+  JWT_ACCESS_LIFESPAN = {"hours": 1}
+  JWT_REFRESH_LIFESPAN = {"days": 14}

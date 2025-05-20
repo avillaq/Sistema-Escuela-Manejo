d5939c84d26fbfc896d6235daa10cad971db76e9
avillaq <villafuertequispealex@gmail.com>
2025-05-20 01:03:49 -0500

    Mejor inicio

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,11 +1,18 @@
 from app import create_app
 from flask import redirect
+from flask import jsonify
 
 app = create_app()
 
 @app.route("/")
 def index_api():
     return redirect("/api")
+
+@app.route("/api")
+def home():
+    return jsonify({
+        "message": "Sistema de escuela de manejo"
+    }), 200
 
 if __name__ == '__main__':
     app.run(debug=True)

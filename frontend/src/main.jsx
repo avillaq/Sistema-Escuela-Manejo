033cedc98fbb773222d85676eec1e285eb4dc1a1
avillaq <villafuertequispealex@gmail.com>
2025-05-27 20:32:02 -0500

    Configura interceptores de Axios y almacén de autenticación en el punto de entrada de la aplicación

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+import { setupInterceptors } from "./service/axiosInstance";
+import { useAuthStore } from "./store/authStore";
+setupInterceptors(useAuthStore);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />

465250c80b4393009f757d5d47c32e0268ed21a4
avillaq <villafuertequispealex@gmail.com>
2025-05-27 22:44:48 -0500

    Elimina comentarios sobre la funcionalidad de actualización de token en los interceptores de Axios

--- frontend/src/service/axiosInstance.js
+++ frontend/src/service/axiosInstance.js
@@ -17,10 +17,7 @@ export const setupInterceptors = (useAuthStore) => {
       if (access_token) {
         const tokenData = jwtDecode(access_token);
         const isExpired = tokenData.exp * 1000 < Date.now();
-        
         if (isExpired) {
-          // As we will not use the refresh functionality of the backend because we 
-          // want a simple code, we will just remove the token from the local storage
           setLogout();
         }
         

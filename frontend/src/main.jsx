8a91a33472ddcceb352d3684f71faddf0a7b0132
avillaq <villafuertequispealex@gmail.com>
2025-06-15 13:19:16 -0500

    Se añade el token en cada peticion al backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { setupInterceptors } from "@/service/axiosInstance";
+import { useAuthStore } from "@/store/auth-store";
+setupInterceptors(useAuthStore);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- frontend/src/service/axiosInstance.js
+++ frontend/src/service/axiosInstance.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import { API_CONFIG } from "@/config/api.config";
-import { useAuthStore } from '@/store/auth-store';
-import { addToast } from '@heroui/react';
 
 export const api = axios.create({
   baseURL: API_CONFIG.baseUrl,
@@ -15,16 +13,17 @@ export const api = axios.create({
 export const setupInterceptors = (useAuthStore) => {
   api.interceptors.request.use(
     async (config) => {
-      const { access_token, setLogout } = useAuthStore.getState();
-  
-      if (access_token) {
-        
-        /*const tokenData = jwtDecode(access_token);
+      const { token, logout } = useAuthStore.getState();
+      console.log(token);
+      
+      if (token) {
+        const tokenData = jwtDecode(token);
         const isExpired = tokenData.exp * 1000 < Date.now();
+
         if (isExpired) {
-          setLogout();
-        }*/
-        config.headers.Authorization = `Bearer ${access_token}`;
+          logout();
+        }
+        config.headers.Authorization = `Bearer ${token}`;
       }
       return config;
     },

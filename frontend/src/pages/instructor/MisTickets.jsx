637e18ee9fc663e1aa9e0f041c6d976e528808b6
avillaq <villafuertequispealex@gmail.com>
2025-06-19 12:55:05 -0500

    Seccion de tickets solo para instructores

---
+++ frontend/src/pages/instructor/MisTickets.jsx
@@ -0,0 +1,8 @@
+export const MisTickets = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">Mis Tickets</h1>
+      <p className="text-gray-600">Esta página está en construcción.</p>
+    </div>
+  );
+}

699fb276ba7263747324907badcc7ec67ef73bad
avillaq <villafuertequispealex@gmail.com>
2025-06-19 12:54:43 -0500

    Seccion de calendario solo para alumnos

---
+++ frontend/src/pages/alumno/MiCalendario.jsx
@@ -0,0 +1,8 @@
+export const MiCalendario = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">Mi Calendario</h1>
+      <p className="text-gray-600">Esta página está en construcción.</p>
+    </div>
+  );
+}
